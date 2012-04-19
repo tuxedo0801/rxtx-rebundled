43898fd71b670b7023af29912d8774b54a9530be
Alex <2862906+tuxedo0801@users.noreply.github.com>
2012-04-19 09:13:29 +0000

    fixed JAR creation

---
+++ trunk/src/main/java/de/root1/rxtxrebundled/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.root1.rxtxrebundled;
+
+/**
+ *
+ * @author achr
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        LibLoader.loadLibrary("rxtxSerial");
+    }
+    
+}
